1e8ddaec5c876dba6496b4b277b9a594c9da9905
sonukumarkushwaha60376@gmail.com <sonukumarkushwaha60376@gmail.com>
2026-07-25 08:04:59 +0530

    nested loop

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
-const favmovie = "avatar";
-let guess  = prompt("guess my favourite movie");
-while((guess != favmovie) && (guess != "quit")){
-    guess = prompt("wrong guess please try again");
+let heroes = [
+    ["ironman","spiderman","thor"],
+    ["superman","wonder woman", "flash"]
+];
+
+for(let i=0; i< heroes.length; i++) {
+    console.log(i, heroes[i], heroes[i].length);
+
+    for(let j=0; j<heroes[i].length; j++){
+        console.log(`j=${j}, ${heroes[i][j]}`);
+    }
 }
-if(guess == favmovie){
-    console.log("congrats!!");
-} else {
-    console.log("you quit");
+let students =[["sakshi",95],["sonu",96],["shardha",100]];
+
+for(let i=0; i<students.length; i++){
+    console.log(`info of students #${i+1}`);
+    for(let j=0; j<students[i].length; j++){
+        console.log(students[i][j]);
+    }
 }
