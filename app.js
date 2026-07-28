b8762248c43ad24b982991d1ea2d714d2fc98c3c
sonukumarkushwaha60376@gmail.com <sonukumarkushwaha60376@gmail.com>
2026-07-28 22:09:56 +0530

    min array

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 let arr =[1,4,2,5,6,7,2,9,2];
 
-let max = arr.reduce((max, el) => {
- if(max < el) {
+let min = arr.reduce((min, el) => {
+ if(min > el) {
     return el;
  } else {
-    return max;
+    return min;
  }
 });
-console.log(max);
+console.log(min);
