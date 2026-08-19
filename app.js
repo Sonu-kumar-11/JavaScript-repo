2a23cb25d331a4309a3be12c9b5cda9b4b342fd9
sonukumarkushwaha60376@gmail.com <sonukumarkushwaha60376@gmail.com>
2026-08-19 22:49:31 +0530

    genrate color

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
-let btns = document.querySelectorAll("button");
+let btns = document.querySelector("button");
 
-for(btn of btns) {
-    // btn.onclick = sayHello;
-    // btn.onclick = sayName;
-    btn.addEventListener("click", sayHello);
-    btn.addEventListener("click", sayName);
+btns.addEventListener("click", function() {
+let h3 = document.querySelector("h3");
+let randomColor = getRandomColor();
+h3.innerText = randomColor;
+
+let div = document.querySelector("div");
+div.style.backgroundColor = randomColor;
+
+console.log("color updated");
+});
+ 
+function getRandomColor() {
+    let red = Math.floor(Math.random()*255);
+     let green = Math.floor(Math.random()*255);
+      let blue = Math.floor(Math.random()*255);
+
+      let color = `rgb(${red},${green},${blue})`;
+      return color;
 }
-
-// btn.onclick = function (){
-//     alert("button was clicked");
-// };
-
-function sayHello(){
-    alert("hello!");
-}
-
-function sayName() {
-    alert("apna college!");
-};
