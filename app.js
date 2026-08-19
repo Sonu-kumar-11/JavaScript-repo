79d8b9ab7b7de193004e300eda4e998c2f9a82c0
sonukumarkushwaha60376@gmail.com <sonukumarkushwaha60376@gmail.com>
2026-08-19 22:02:56 +0530

    event listener

--- app.js
+++ app.js
@@ -1,23 +1,20 @@
-let para1 = document.createElement("p");
-para1.innerText = "Hey I'm red!";
-document.querySelector("body").prepend(para1);
+let btns = document.querySelectorAll("button");
 
-para1.classList.add("red");
+for(btn of btns) {
+    // btn.onclick = sayHello;
+    // btn.onclick = sayName;
+    btn.addEventListener("click", sayHello);
+    btn.addEventListener("click", sayName);
+}
 
-let h3 = document.createElement("h3");
-h3.innerText = "Hey I'm a blue H3!";
-document.querySelector("body").prepend(h3);
+// btn.onclick = function (){
+//     alert("button was clicked");
+// };
 
-h3.classList.add("blue");
+function sayHello(){
+    alert("hello!");
+}
 
-let div = document.createElement("div");
-let h1 = document.createElement("h1");
-let para = document.createElement("p");
-
-h1.innerText = "i'm in a div";
-para.innerText ="Me Too!";
-
-div.append(h1);
-div.append(para);
-div.classList.add("box");
-document.querySelector("body").prepend(div);
+function sayName() {
+    alert("apna college!");
+};
