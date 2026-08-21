fdbd379bd0149b3d8613e12c313236ea4b28b174
sonukumarkushwaha60376@gmail.com <sonukumarkushwaha60376@gmail.com>
2026-08-21 21:26:25 +0530

    todo app

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
-let div = document.querySelector("div");
+let btn = document.querySelector("button");
 let ul = document.querySelector("ul");
-let lis = document.querySelectorAll("li");
+let inp = document.querySelector("input");
 
-div.addEventListener("click",function(){
-    console.log("div was clicked");
+btn.addEventListener("click",function() {
+    let item = document.createElement("li");
+    item.innerText = inp.value;
+
+    let delBtn =document.createElement("button");
+    delBtn.innerText = "delete";
+    delBtn.classList.add("delete");
+
+    item.appendChild(delBtn);
+    ul.appendChild(item);
+    inp.value= "";
+
+    let delBtns = document.querySelectorAll(".delete");
+    for(delBtn of delBtns) {
+        delBtn.addEventListener("click", function() {
+            let par = this.parentElement;
+            console.log(par);
+            par.remove();
+        });
+    }
 });
 
-ul.addEventListener("click",function(event){
-    event.stopPropagation();
-    console.log("ul was clicked");
-});
-
-for(li of lis){
-    li.addEventListener("click",function(event){
-        event.stopPropagation();
-        console.log("li was clicked");
-    });
-}
